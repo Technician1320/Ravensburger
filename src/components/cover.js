cca7be6b2d0a503688d116a969cf5c20eba4baf5
Marc <purotx2014@gmail.com>
2019-12-21 09:42:18 -0500

    added code

---
+++ src/components/cover.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cover = () => {
+	return (			
+		<div className='container'>
+			<div className='row'>
+				<h2 className='header col s12 center'>Only click the picture one time!</h2>
+			</div>
+		</div>	
+	);
+};
+
+export default Cover;

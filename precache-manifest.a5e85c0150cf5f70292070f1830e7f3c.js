79049574b7313ef4dff5dd9cea876743ff1eaed5
Marc <purotx2014@gmail.com>
2019-12-21 10:20:54 -0500

    Updates

---
+++ precache-manifest.a5e85c0150cf5f70292070f1830e7f3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2bf26c228ef26c72825e73462f158a",
+    "url": "/ravensburger/index.html"
+  },
+  {
+    "revision": "a5b197ec063a0ca4d50b",
+    "url": "/ravensburger/static/js/2.e1de016b.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/ravensburger/static/js/2.e1de016b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "312eb7b2dae140d4f9f7",
+    "url": "/ravensburger/static/js/main.2f72b2fd.chunk.js"
+  },
+  {
+    "revision": "0da395322d1ed1b2f2da",
+    "url": "/ravensburger/static/js/runtime-main.5211ea2a.js"
+  }
+]);
